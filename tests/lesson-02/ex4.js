35fb24512aff2b3078c1bcb3d3dd8769fd6027ff
alehoang <anhlh.90@gmail.com>
2026-01-20 21:10:00 +0700

    feat: add lesson-02

---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,6 @@
+const height = 170;
+
+const idealWeight = (height - 100) * 9 / 10;
+const maxWeight = height - 100;
+const minWeight = (height - 100) * 8 / 10;
+console.log(`Ideal weight is ${idealWeight}; Max weight is ${maxWeight}; Min weight is ${minWeight}`);
